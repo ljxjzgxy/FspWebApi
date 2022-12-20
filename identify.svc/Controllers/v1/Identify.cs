30b563d0b8c5a56fadc3aa4de922169b35d3243e
hw <hw@gmail.com>
2022-12-20 10:15:42 -0500

    test

--- identify.svc/Controllers/v1/Identify.cs
+++ identify.svc/Controllers/v1/Identify.cs
@@ -13,7 +13,7 @@ public class IdentifyController : ApiControllerBaseV1
         return new ApiResult<string>()
         {
             Success = true,
-            Message = "test auto deploy",
+            Message = "test auto deploy 02",
             Data = _jwtOption.Value.Key
 
         };
