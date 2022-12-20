720f59c4677935ec78e8377561c12db1fbb41586
hw <hw@gmail.com>
2022-12-20 12:46:44 -0500

    revert swagger

--- fsp.lib/DependencyInjection/Swagger.cs
+++ fsp.lib/DependencyInjection/Swagger.cs
@@ -77,7 +77,7 @@ namespace fsp.lib.DependencyInjection
             {
                 foreach (var version in typeof(ApiVersions).GetEnumNames())
                 {
-                    options.SwaggerEndpoint($"./swagger/{version}/swagger.json", $"{version}");
+                    options.SwaggerEndpoint($"/swagger/{version}/swagger.json", $"{version}");
                 }
             });
         }
