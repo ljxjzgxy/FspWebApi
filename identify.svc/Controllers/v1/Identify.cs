b64b44cb01efe11effcadd2b100c22d6dedfac16
hw <hw@gmail.com>
2022-12-20 14:02:08 -0500

    ff

--- identify.svc/Controllers/v1/Identify.cs
+++ identify.svc/Controllers/v1/Identify.cs
@@ -13,7 +13,7 @@ public class IdentifyController : ApiControllerBaseV1
         return new ApiResult<string>()
         {
             Success = true,
-            Message = "test auto deploy swagger 500",
+            Message = "test auto deploy swagger 501",
             Data = _jwtOption.Value.Key
 
         };
